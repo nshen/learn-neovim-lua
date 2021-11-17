71e82cd1976ce320aa767cf38f1b29f42ba10f28
Xin.Chen <xin.chen@percent.cn>
2021-11-17 19:03:46 +0800

    👌 IMPROVE: basic.lua

--- lua/basic.lua
+++ lua/basic.lua
@@ -33,24 +33,24 @@ print(vim.inspect(vim.opt.autoindent))
 -- utf8
 vim.g.encoding = "UTF-8"
 vim.o.fileencoding = 'utf-8'
--- 光标下方保留8行
+-- jk移动时光标下上方保留8行
 vim.o.scrolloff = 8
 vim.o.sidescrolloff = 8
--- 相对行号
+-- 使用相对行号
 vim.wo.number = true
 vim.wo.relativenumber = true
 -- 高亮所在行
 vim.wo.cursorline = true
 -- 显示左侧图标指示列
 vim.wo.signcolumn = "yes"
--- 右侧参考线
+-- 右侧参考线，超过表示代码太长了，考虑换行
 vim.wo.colorcolumn = "80"
 -- 缩进2个空格等于一个Tab
 vim.o.tabstop = 2
 vim.bo.tabstop = 2
 vim.o.softtabstop = 2
 vim.o.shiftround = true
--- >> << 时长度
+-- >> << 时移动长度
 vim.o.shiftwidth = 2
 vim.bo.shiftwidth = 2
 -- 新行对齐当前行，空格替代tab
@@ -76,12 +76,12 @@ vim.bo.autoread = true
 -- 禁止折行
 vim.o.wrap = false
 vim.wo.wrap = false
+-- 行结尾可以跳到下一行
+vim.o.whichwrap = 'b,s,<,>,[,],h,l'
 -- 允许隐藏被修改过的buffer
 vim.o.hidden = true
 -- 鼠标支持
 vim.o.mouse = "a"
--- 行结尾可以跳到下一行
-vim.o.whichwrap = 'b,s,<,>,[,],h,l'
 -- 禁止创建备份文件
 vim.o.backup = false
 vim.o.writebackup = false
@@ -95,20 +95,19 @@ vim.o.splitbelow = true
 vim.o.splitright = true
 -- 自动补全不自动选中
 vim.g.completeopt = "menu,menuone,noselect,noinsert"
----------------------------------------------
+-- 样式
 vim.o.background = "dark"
 vim.o.termguicolors = true
 vim.opt.termguicolors = true
---? tab 字符显示
+-- 不可见字符的显示，这里只把空格显示为一个点
 vim.o.list = true
-vim.o.listchars = "tab:>·"
--- 补全
+vim.o.listchars = "space:·"
+-- 补全增强
 vim.o.wildmenu = true
 -- Dont' pass messages to |ins-completin menu|
 vim.o.shortmess = vim.o.shortmess .. 'c'
 vim.o.pumheight = 10
--- vim.o.conceallevel = 0
+-- always show tabline
 vim.o.showtabline = 2
--- vim.o.clipboard = "unnamedplus"
 
 
