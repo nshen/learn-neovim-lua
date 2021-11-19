8f7587e2741ef38a38a913e59fdf5f1ae50bba6b
Xin.Chen <xin.chen@percent.cn>
2021-11-19 19:32:01 +0800

    👌 IMPROVE: update keybinding config

--- lua/plugin-config/comment.lua
+++ lua/plugin-config/comment.lua
@@ -1,13 +1,1 @@
- require('Comment').setup({
-  -- normal 模式
-  toggler = {
-    line = '<leader>c',
-    block = '<leader>bc',
-  },
-  -- visual 模式
-  opleader = {
-    -- ctrl + / 
-    line = '<C-_>',
-    block = 'bc',
-  },
- })
+require('Comment').setup(require('keybindings').comment)
