ebe485a3e99fcdab28249dfa1a82f7291e79fcab
Xin.Chen <xin.chen@percent.cn>
2021-12-09 20:49:52 +0800

    👌 IMPROVE: bufferline indicator

--- lua/plugin-config/bufferline.lua
+++ lua/plugin-config/bufferline.lua
@@ -1,15 +1,30 @@
+-- https://github.com/akinsho/bufferline.nvim
 vim.opt.termguicolors = true
 require("bufferline").setup {
-    options = {
-        -- 使用 nvim 内置lsp
-        diagnostics = "nvim_lsp",
-        -- 左侧让出 nvim-tree 的位置
-        offsets = {{
-            filetype = "NvimTree",
-            text = "File Explorer",
-            highlight = "Directory",
-            text_align = "left"
-        }}
-    }
+  options = {
+    -- 使用 nvim 内置lsp
+    diagnostics = "nvim_lsp",
+    -- 左侧让出 nvim-tree 的位置
+    offsets = {{
+      filetype = "NvimTree",
+      text = "File Explorer",
+      highlight = "Directory",
+      text_align = "left"
+    }},
+    -- 显示lsp报错图标
+    ---@diagnostic disable-next-line: unused-local
+    diagnostics_indicator = function(count, level, diagnostics_dict, context)
+      local s = " "
+      -- if context.buffer:current() then
+      --   return s
+      -- end
+      for e, n in pairs(diagnostics_dict) do
+        local sym = e == "error" and " "
+          or (e == "warning" and " " or "" )
+        s = s .. n .. sym
+      end
+      return s
+    end
+  }
 }
 
