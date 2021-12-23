5cfc97f42556100b1971cc34f604da9c8ce0c6f0
Xin.Chen <xin.chen@percent.cn>
2021-12-23 14:24:03 +0800

    👌 IMPROVE: indent-blankline

--- lua/plugin-config/indent-blankline.lua
+++ lua/plugin-config/indent-blankline.lua
@@ -4,12 +4,24 @@
 -- vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
 -- vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
 -- vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+-- vim.cmd [[ let g:indent_blankline_char = '¦' ]]
 
 require("indent_blankline").setup {
-  -- for example, context is off by default, use this to turn it on
+  -- 空行占位
+  space_char_blankline = " ",
+  -- 用 treesitter 判断上下文
   show_current_context = true,
   show_current_context_start = true,
-  -- space_char_blankline = " ",
+  -- 使用 char
+  -- char = '¦'
+  -- char = '┆'
+  -- char = '│'
+  -- char = '⎸'
+  char = '▏'
+
+  -- 不同级别使用不同char
+  -- char_list = {'|', '¦', '┆', '┊'}
+  -- 自定义颜色
   -- char_highlight_list = {
   --   "IndentBlanklineIndent1",
   --   "IndentBlanklineIndent2",
