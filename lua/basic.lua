8571f26074412047584b8acf7d4c34bd7b3733da
Xin.Chen <xin.chen@percent.cn>
2021-12-29 15:43:30 +0800

    👌 IMPROVE: basic.lua

--- lua/basic.lua
+++ lua/basic.lua
@@ -1,7 +1,7 @@
 -- utf8
 vim.g.encoding = "UTF-8"
 vim.o.fileencoding = 'utf-8'
--- jk移动时光标下上方保留8行
+-- jkhl 移动时光标周围保留8行
 vim.o.scrolloff = 8
 vim.o.sidescrolloff = 8
 -- 使用相对行号
@@ -21,9 +21,10 @@ vim.o.shiftround = true
 -- >> << 时移动长度
 vim.o.shiftwidth = 2
 vim.bo.shiftwidth = 2
--- 新行对齐当前行，空格替代tab
+-- 空格替代tab
 vim.o.expandtab = true
 vim.bo.expandtab = true
+-- 新行对齐当前行
 vim.o.autoindent = true
 vim.bo.autoindent = true
 vim.o.smartindent = true
@@ -40,10 +41,9 @@ vim.o.cmdheight = 2
 vim.o.autoread = true
 vim.bo.autoread = true
 -- 禁止折行
-vim.o.wrap = false
 vim.wo.wrap = false
--- 行结尾可以跳到下一行
-vim.o.whichwrap = 'b,s,<,>,[,],h,l'
+-- 光标在行首尾时<Left><Right>可以跳到下一行
+vim.o.whichwrap = '<,>,[,]'
 -- 允许隐藏被修改过的buffer
 vim.o.hidden = true
 -- 鼠标支持
