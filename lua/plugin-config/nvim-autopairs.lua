4304fe553cb0447320d2959da493e8d11cfcc7d5
Xin.Chen <xin.chen@percent.cn>
2022-01-06 15:48:00 +0800

    👌 IMPROVE: update autopairs config

--- lua/plugin-config/nvim-autopairs.lua
+++ lua/plugin-config/nvim-autopairs.lua
@@ -1,5 +1,12 @@
 -- https://github.com/windwp/nvim-autopairs
-require("nvim-autopairs").setup({})
+require("nvim-autopairs").setup({
+  check_ts = true,
+  ts_config = {
+    lua = { "string" }, -- it will not add a pair on that treesitter node
+    javascript = { "template_string" },
+    java = false, -- don't check treesitter on java
+  },
+})
 -- If you want insert `(` after select function or method item
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 local cmp = require("cmp")
