7c22f43ddf7c9953f58a8d5f3b25d554844c3e2e
Xin.Chen <xin.chen@percent.cn>
2022-01-08 13:40:47 +0800

    🐛 FIX: fix telescope esc

--- lua/plugin-config/telescope.lua
+++ lua/plugin-config/telescope.lua
@@ -5,6 +5,7 @@ require("telescope").setup({
     -- config_key = value,
     mappings = {
       i = {
+        -- ["<esc>"] = actions.close,
         ["<C-n>"] = actions.cycle_history_next,
         ["<C-p>"] = actions.cycle_history_prev,
         ["<C-j>"] = actions.move_selection_next,
