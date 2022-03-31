20ef34712474068f41a7ec3b75d7df55760e78a6
nshen <nshen121@gmail.com>
2022-03-31 20:53:03 +0800

    update nvim-tree config

--- lua/plugin-config/nvim-tree.lua
+++ lua/plugin-config/nvim-tree.lua
@@ -1,8 +1,8 @@
 -- https://github.com/kyazdani42/nvim-tree.lua
 -- local nvim_tree = require'nvim-tree'
-local status, nvim_tree = pcall(require, "nvim-tree")
+local status, nvim_tree = pcall(require, 'nvim-tree')
 if not status then
-    vim.notify("没有找到 nvim-tree")
+    vim.notify('没有找到 nvim-tree')
     return
 end
 
@@ -10,46 +10,50 @@ end
 local list_keys = require('keybindings').nvimTreeList
 
 nvim_tree.setup({
-    -- 关闭文件时自动关闭
-    auto_close = true,
     -- 不显示 git 状态图标
     git = {
-        enable = false
+        enable = false,
     },
     -- project plugin 需要这样设置
     update_cwd = true,
     update_focused_file = {
         enable = true,
-        update_cwd = true
+        update_cwd = true,
     },
     -- 隐藏 .文件 和 node_modules 文件夹
     filters = {
         dotfiles = true,
-        custom = {"node_modules"}
+        custom = { 'node_modules' },
     },
     view = {
         -- 宽度
-        width = 30,
+        width = 40,
         -- 也可以 'right'
-        side = "left",
+        side = 'left',
         -- 隐藏根目录
         hide_root_folder = false,
-        -- 打开第一个文件自动resize
-        auto_resize = true,
         -- 自定义列表中快捷键
         mappings = {
             custom_only = false,
-            list = list_keys
+            list = list_keys,
         },
         -- 不显示行数
         number = false,
         relativenumber = false,
         -- 显示图标
-        signcolumn = "yes"
+        signcolumn = 'yes',
+    },
+    actions = {
+        open_file = {
+            -- 首次打开大小适配
+            resize_window = true,
+            -- 打开文件时关闭 tree
+            quit_on_open = true,
+        },
     },
     -- wsl install -g wsl-open
     -- https://github.com/4U6U57/wsl-open/
     system_open = {
-        cmd = "wsl-open"
-    }
+        cmd = 'open',
+    },
 })
