f191ee1a5b34065206f507cc10737f4b8ac47e2e
nshen <nshen121@gmail.com>
2022-04-07 19:44:23 +0800

    im-select mac support

--- lua/utils/im-select.lua
+++ lua/utils/im-select.lua
@@ -1,10 +1,26 @@
-vim.cmd([[
-" 1033 中文语言
-" 2052 英文语言
-augroup im_select
-    autocmd!
-    autocmd InsertLeave * silent ! ~/.config/nvim/im-select.exe 1033
-    autocmd InsertEnter * silent !~/.config/nvim/im-select.exe 2052
-    autocmd BufRead * silent !~/.config/nvim/im-select.exe 1033
-augroup END
-]])
+local M = {}
+
+M.defaultIM = "com.apple.keylayout.ABC"
+M.currentIM = M.defaultIM
+
+M.macInsertLeave = function()
+  M.currentIM = vim.fn.system({ "im-select" })
+  vim.cmd(":silent :!im-select" .. " " .. M.defaultIM)
+end
+
+M.macInsertEnter = function()
+  if M.currentIM then
+    vim.cmd(":silent :!im-select" .. " " .. M.currentIM)
+  else
+    vim.cmd(":silent :!im-select" .. " " .. M.defaultIM)
+  end
+end
+
+M.windowsInsertLeave = function()
+  vim.cmd(":silent :!~/.config/nvim/im-select.exe 1033")
+end
+
+M.windowsInsertEnter = function()
+  vim.cmd(":silent :!~/.config/nvim/im-select.exe 2052")
+end
+return M
