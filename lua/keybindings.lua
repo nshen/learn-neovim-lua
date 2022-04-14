1c9467895490f8a37723f657b5e52352127dc974
nshen <nshen121@gmail.com>
2022-04-14 23:58:44 +0800

    use leader q to quit all

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -54,7 +54,7 @@ map("v", "p", '"_dP', opt)
 -- 退出
 map("n", "q", ":q<CR>", opt)
 map("n", "qq", ":q!<CR>", opt)
-map("n", "Q", ":qa!<CR>", opt)
+map("n", "<leader>q", ":qa!<CR>", opt)
 
 -- insert 模式下，跳到行首行尾
 -- map("i", "<C-h>", "<ESC>I", opt)
