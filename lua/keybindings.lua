93b742ef93a4c663978ef4a6705f1a88312e1857
nshen <nshen121@gmail.com>
2022-04-18 13:32:00 +0800

    Fix: fix comment plugin breaking change

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -186,10 +186,12 @@ pluginKeys.telescopeList = {
 -- 代码注释插件
 -- see ./lua/plugin-config/comment.lua
 pluginKeys.comment = {
+  -- Normal 模式快捷键
   toggler = {
-    line = "gcc",
-    block = "gbc",
+    line = "gcc", -- 行注释
+    block = "gbc", -- 块注释
   },
+  -- Visual 模式
   opleader = {
     line = "gc",
     bock = "gb",
--- lua/plugin-config/comment.lua
+++ lua/plugin-config/comment.lua
@@ -4,4 +4,75 @@ if not status then
   return
 end
 
-comment.setup(require('keybindings').comment)
+local default_opts = {
+  ---Add a space b/w comment and the line
+  ---@type boolean|fun():boolean
+  padding = true,
+
+  ---Whether the cursor should stay at its position
+  ---NOTE: This only affects NORMAL mode mappings and doesn't work with dot-repeat
+  ---@type boolean
+  sticky = true,
+
+  ---Lines to be ignored while comment/uncomment.
+  ---Could be a regex string or a function that returns a regex string.
+  ---Example: Use '^$' to ignore empty lines
+  ---@type string|fun():string
+  ignore = nil,
+
+  ---LHS of toggle mappings in NORMAL + VISUAL mode
+  ---@type table
+  toggler = {
+    ---Line-comment toggle keymap
+    line = "gcc",
+    ---Block-comment toggle keymap
+    block = "gbc",
+  },
+
+  ---LHS of operator-pending mappings in NORMAL + VISUAL mode
+  ---@type table
+  opleader = {
+    ---Line-comment keymap
+    line = "gc",
+    ---Block-comment keymap
+    block = "gb",
+  },
+
+  ---LHS of extra mappings
+  ---@type table
+  extra = {
+    ---Add comment on the line above
+    above = "gcO",
+    ---Add comment on the line below
+    below = "gco",
+    ---Add comment at the end of line
+    eol = "gcA",
+  },
+
+  ---Create basic (operator-pending) and extended mappings for NORMAL + VISUAL mode
+  ---NOTE: If `mappings = false` then the plugin won't create any mappings
+  ---@type boolean|table
+  mappings = {
+    ---Operator-pending mapping
+    ---Includes `gcc`, `gbc`, `gc[count]{motion}` and `gb[count]{motion}`
+    ---NOTE: These mappings can be changed individually by `opleader` and `toggler` config
+    basic = true,
+    ---Extra mapping
+    ---Includes `gco`, `gcO`, `gcA`
+    extra = false,
+    ---Extended mapping
+    ---Includes `g>`, `g<`, `g>[count]{motion}` and `g<[count]{motion}`
+    extended = false,
+  },
+
+  ---Pre-hook, called before commenting the line
+  ---@type fun(ctx: Ctx):string
+  pre_hook = nil,
+
+  ---Post-hook, called after commenting is done
+  ---@type fun(ctx: Ctx)
+  post_hook = nil,
+}
+
+-- 关闭了extra快捷键，只用keybindings里定义的基础快捷键
+comment.setup(vim.tbl_deep_extend("force", default_opts, require("keybindings").comment))
