5668efa4fcf0aad2a94f463dc3e54d807b6a6356
nshen <nshen121@gmail.com>
2022-04-19 21:49:34 +0800

    fix bash lsp

--- lua/lsp/config/bash.lua
+++ lua/lsp/config/bash.lua
@@ -1,5 +1,18 @@
 return {
   on_setup = function(server)
-    server:setup({})
+    server:setup({
+      capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+      flags = {
+        debounce_text_changes = 150,
+      },
+      on_attach = function(client, bufnr)
+        client.resolved_capabilities.document_formatting = false
+        client.resolved_capabilities.document_range_formatting = false
+        local function buf_set_keymap(...)
+          vim.api.nvim_buf_set_keymap(bufnr, ...)
+        end
+        require("keybindings").mapLSP(buf_set_keymap)
+      end,
+    })
   end,
 }
