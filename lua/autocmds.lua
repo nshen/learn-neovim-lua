f24f155a47a4d60d3ac7500bd6d28055fbb3aeaa
nshen <nshen121@gmail.com>
2022-04-19 21:50:14 +0800

    autoformat bash

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -34,7 +34,7 @@ autocmd("TermOpen", {
 -- 保存时自动格式化
 autocmd("BufWritePre", {
   group = myAutoGroup,
-  pattern = { "*.lua", "*.py" },
+  pattern = { "*.lua", "*.py", "*.sh" },
   callback = vim.lsp.buf.formatting_sync,
 })
 
