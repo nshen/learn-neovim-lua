1cf95b8c7508ef787c0a4d8e8f87374fa573afee
nshen <nshen121@gmail.com>
2022-04-19 11:02:22 +0800

    use lualine globalstatus

--- lua/plugin-config/lualine.lua
+++ lua/plugin-config/lualine.lua
@@ -1,51 +1,52 @@
-local status, lualine = pcall(require, 'lualine')
+local status, lualine = pcall(require, "lualine")
 if not status then
-    vim.notify('没有找到 lualine')
-    return
+  vim.notify("没有找到 lualine")
+  return
 end
 
 lualine.setup({
-    options = {
-        -- 指定皮肤
-        -- https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
-        theme = 'tokyonight',
-        -- 分割线
-        component_separators = {
-            left = '|',
-            right = '|',
-        },
-        -- https://github.com/ryanoasis/powerline-extra-symbols
-        section_separators = {
-            left = ' ',
-            right = '',
-        },
+  options = {
+    -- 指定皮肤
+    -- https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
+    theme = "tokyonight",
+    -- 分割线
+    component_separators = {
+      left = "|",
+      right = "|",
     },
-    extensions = { 'nvim-tree' },
-    sections = {
-        lualine_c = {
-            'filename',
-            {
-                'lsp_progress',
-                spinner_symbols = { ' ', ' ', ' ', ' ', ' ', ' ' },
-            },
-        },
-        lualine_x = {
-            'filesize',
-            {
-                'fileformat',
-                -- symbols = {
-                --   unix = '', -- e712
-                --   dos = '', -- e70f
-                --   mac = '', -- e711
-                -- },
-                symbols = {
-                    unix = 'LF',
-                    dos = 'CRLF',
-                    mac = 'CR',
-                },
-            },
-            'encoding',
-            'filetype',
-        },
+    -- https://github.com/ryanoasis/powerline-extra-symbols
+    section_separators = {
+      left = " ",
+      right = "",
     },
+    globalstatus = true,
+  },
+  extensions = { "nvim-tree" },
+  sections = {
+    lualine_c = {
+      "filename",
+      {
+        "lsp_progress",
+        spinner_symbols = { " ", " ", " ", " ", " ", " " },
+      },
+    },
+    lualine_x = {
+      "filesize",
+      {
+        "fileformat",
+        -- symbols = {
+        --   unix = '', -- e712
+        --   dos = '', -- e70f
+        --   mac = '', -- e711
+        -- },
+        symbols = {
+          unix = "LF",
+          dos = "CRLF",
+          mac = "CR",
+        },
+      },
+      "encoding",
+      "filetype",
+    },
+  },
 })
