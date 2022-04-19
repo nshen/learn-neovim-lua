25f914a007a40c6e31eb941d06ffa2b5d6fda13c
nshen <nshen121@gmail.com>
2022-04-19 11:09:49 +0800

    format project.lua

--- lua/plugin-config/project.lua
+++ lua/plugin-config/project.lua
@@ -4,21 +4,21 @@
 -- 我的文件在
 --  ~/.local/share/nvim/project_nvim/project_history
 
-local status, project = pcall(require, 'project_nvim')
+local status, project = pcall(require, "project_nvim")
 if not status then
-    vim.notify('没有找到 project_nvim')
-    return
+  vim.notify("没有找到 project_nvim")
+  return
 end
 
 vim.g.nvim_tree_respect_buf_cwd = 1
 project.setup({
-    detection_methods = { 'pattern' },
-    patterns = { '.git', '_darcs', '.hg', '.bzr', '.svn', 'Makefile', 'package.json', '.sln' },
+  detection_methods = { "pattern" },
+  patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", ".sln" },
 })
 
-local status, telescope = pcall(require, 'telescope')
+local status, telescope = pcall(require, "telescope")
 if not status then
-    vim.notify('没有找到 telescope')
-    return
+  vim.notify("没有找到 telescope")
+  return
 end
-pcall(telescope.load_extension, 'projects')
+pcall(telescope.load_extension, "projects")
