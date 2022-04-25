18fe3947671088bc70a7ef75d6d2ca82c7ac0444
nshen <nshen121@gmail.com>
2022-04-25 19:37:18 +0800

    cmp cmdline

--- lua/lsp/cmp.lua
+++ lua/lsp/cmp.lua
@@ -52,6 +52,7 @@ cmp.setup.cmdline("/", {
 
 -- Use cmdline & path source for ':'.
 cmp.setup.cmdline(":", {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
     { name = "path" },
   }, {
