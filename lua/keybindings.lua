78b11c8971e5d1043ace5a233b3707db0c9261cf
nshen <nshen121@gmail.com>
2022-05-02 09:41:16 +0800

    Fix toggleterm config path

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -374,7 +374,13 @@ pluginKeys.cmp = function(cmp)
   }
 end
 
-local toggleterm = require("lua.plugin-config.toggleterm")
+-- 自定义 toggleterm 3个不同类型的命令行窗口
+-- <leader>ta 浮动
+-- <leader>tb 右侧
+-- <leader>tc 下方
+-- 特殊lazygit 窗口，需要安装lazygit
+-- <leader>tg lazygit
+local toggleterm = require("plugin-config.toggleterm")
 vim.keymap.set({ "n", "t" }, "<leader>ta", toggleterm.toggleA)
 vim.keymap.set({ "n", "t" }, "<leader>tb", toggleterm.toggleB)
 vim.keymap.set({ "n", "t" }, "<leader>tc", toggleterm.toggleC)
