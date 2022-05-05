f024e8428549e38f60af0b963440e099f51eb030
nshen <nshen121@gmail.com>
2022-05-05 21:55:54 +0800

    fix nvim-tree

--- lua/plugin-config/nvim-tree.lua
+++ lua/plugin-config/nvim-tree.lua
@@ -10,6 +10,8 @@ end
 local list_keys = require("keybindings").nvimTreeList
 
 nvim_tree.setup({
+  -- 完全禁止内置netrw
+  disable_netrw = true,
   -- 不显示 git 状态图标
   git = {
     enable = false,
@@ -27,14 +29,14 @@ nvim_tree.setup({
   },
   view = {
     -- 宽度
-    width = 36,
+    width = 34,
     -- 也可以 'right'
     side = "left",
     -- 隐藏根目录
     hide_root_folder = false,
     -- 自定义列表中快捷键
     mappings = {
-      custom_only = true,
+      custom_only = false,
       list = list_keys,
     },
     -- 不显示行数
