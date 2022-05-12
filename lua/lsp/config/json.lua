b7e01a326abedefbe1323525c62fb0e870acac49
nshen <nshen121@gmail.com>
2022-05-12 09:41:52 +0800

    fix json lsp config

--- lua/lsp/config/json.lua
+++ lua/lsp/config/json.lua
@@ -10,10 +10,15 @@ return {
       flags = {
         debounce_text_changes = 150,
       },
-      on_attach = function(client, _)
+      on_attach = function(client, bufnr)
         -- 禁用格式化功能，交给专门插件插件处理
         client.resolved_capabilities.document_formatting = false
         client.resolved_capabilities.document_range_formatting = false
+        -- 绑定快捷键
+        local function buf_set_keymap(...)
+          vim.api.nvim_buf_set_keymap(bufnr, ...)
+        end
+        require("keybindings").mapLSP(buf_set_keymap)
       end,
     })
   end,
