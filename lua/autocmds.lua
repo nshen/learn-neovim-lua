0d7609540dbd7e62ba3ee64402f9db316777a4d9
nshen <nshen121@gmail.com>
2022-05-13 00:33:05 +0800

    set formatoptions-=o

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -59,3 +59,14 @@ autocmd("TextYankPost", {
   group = myAutoGroup,
   pattern = "*",
 })
+
+-- 用o换行不要延续注释
+autocmd("BufEnter", {
+  group = myAutoGroup,
+  pattern = "*",
+  callback = function()
+    vim.opt.formatoptions = vim.opt.formatoptions
+      - "o" -- O and o, don't continue comments
+      + "r" -- But do continue when pressing enter.
+  end,
+})
