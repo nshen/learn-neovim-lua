ed0604fb1cfd6a01960c8cb3d02b93d2dbd352a2
nshen <nshen121@gmail.com>
2022-05-16 21:31:50 +0800

    fix: fidget should ignore null-ls

--- lua/plugin-config/fidget.lua
+++ lua/plugin-config/fidget.lua
@@ -4,4 +4,16 @@ if not status then
   return
 end
 
-fidget.setup({})
+fidget.setup({
+  text = {
+    spinner = "zip", -- animation shown when tasks are ongoing
+    done = "✔", -- character shown when all tasks are complete
+    commenced = "Started", -- message shown when task starts
+    completed = "Completed", -- message shown when task completes
+  },
+  sources = {
+    ["null-ls"] = {
+      ignore = true,
+    },
+  },
+})
