a59db394b39a82f37701e04af12ba0c7acd76620
nshen <nshen121@gmail.com>
2022-08-07 15:16:10 +0800

    toggleterm send cmd

--- lua/plugin-config/toggleterm.lua
+++ lua/plugin-config/toggleterm.lua
@@ -61,7 +61,7 @@ local tc = Terminal:new({
 
 local M = {}
 
-M.toggleA = function()
+M.toggleA = function(cmd)
   if ta:is_open() then
     ta:close()
     return
@@ -69,6 +69,9 @@ M.toggleA = function()
   tb:close()
   tc:close()
   ta:open()
+  if cmd ~= nil then
+    ta:send(cmd)
+  end
 end
 
 M.toggleB = function()
