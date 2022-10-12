55733c5fd864d347f84993cdb9921bcf78fcaa34
nshen <nshen121@gmail.com>
2022-10-13 00:57:51 +0800

    update lsp ui config

--- lua/lsp/ui.lua
+++ lua/lsp/ui.lua
@@ -3,6 +3,19 @@ vim.diagnostic.config({
   virtual_text = true,
   signs = true,
   update_in_insert = false,
+  underline = true,
+  show_header = false,
+  severity_sort = true,
+  float = {
+    source = "always",
+    border = "rounded",
+    style = "minimal",
+    header = "",
+    -- prefix = " ",
+    -- max_width = 100,
+    -- width = 60,
+    -- height = 20,
+  },
 })
 
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
@@ -10,57 +23,3 @@ for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
-
-local lspsaga = require("lspsaga")
-lspsaga.setup({ -- defaults ...
-  debug = false,
-  use_saga_diagnostic_sign = true,
-  -- diagnostic sign
-  error_sign = "",
-  warn_sign = "",
-  hint_sign = "",
-  infor_sign = "",
-  diagnostic_header_icon = "   ",
-  -- code action title icon
-  code_action_icon = " ",
-  code_action_prompt = {
-    enable = false,
-    sign = true,
-    sign_priority = 40,
-    virtual_text = false,
-  },
-  finder_definition_icon = "  ",
-  finder_reference_icon = "  ",
-  max_preview_lines = 10,
-  finder_action_keys = {
-    -- open = "o",
-    open = "<CR>",
-    vsplit = "s",
-    split = "i",
-    -- quit = "q",
-    quit = "<ESC>",
-    scroll_down = "<C-f>",
-    scroll_up = "<C-b>",
-  },
-  code_action_keys = {
-    -- quit = "q",
-    quit = "<ESC>",
-    exec = "<CR>",
-  },
-  rename_action_keys = {
-    -- quit = "<C-c>",
-    quit = "<ESC>",
-    exec = "<CR>",
-  },
-  definition_preview_icon = "  ",
-  border_style = "single",
-  rename_prompt_prefix = "➤",
-  rename_output_qflist = {
-    enable = false,
-    auto_open_qflist = false,
-  },
-  server_filetype_map = {},
-  diagnostic_prefix_format = "%d. ",
-  diagnostic_message_format = "%m %c",
-  highlight_prefix = false,
-})
