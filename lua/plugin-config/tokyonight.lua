242de2d19cb43e83886214f247a40e3e8cdc7a46
nshen <nshen121@gmail.com>
2022-10-16 20:52:05 +0800

    fix require

--- lua/plugin-config/tokyonight.lua
+++ lua/plugin-config/tokyonight.lua
@@ -1,4 +1,9 @@
-require("tokyonight").setup({
-  style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-  transparent = false, -- Enable this to disable setting the background color
+local status, tokyonight = pcall(require, "tokyonight")
+if not status then
+    vim.notify("没有找到 tokyonight")
+    return
+end
+tokyonight.setup({
+    style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+    transparent = false -- Enable this to disable setting the background color
 })
