1ccf718da719214f9519b857da3e497b1a5add88
lzr <3200348589@qq.com>
2022-10-29 12:02:26 +0800

    add lsp: clangd && cmake using default configuration

---
+++ lua/lsp/config/clangd.lua
@@ -0,0 +1,15 @@
+local common = require("lsp.common-config")
+local opts = {
+  capabilities = common.capabilities,
+  flags = common.flags,
+  on_attach = function(client, bufnr)
+    common.disableFormat(client)
+    common.keyAttach(bufnr)
+  end,
+}
+
+return {
+  on_setup = function(server)
+    server.setup(opts)
+  end,
+}
